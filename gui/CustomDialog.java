3c34a46f2e35ddc9cf3bc59bbfa14bd467fdcddf
IamMKey <molnar.konrad@gmail.com>
2025-04-18 14:04:24 +0200

    Update CustomDialog.java

--- gui/CustomDialog.java
+++ gui/CustomDialog.java
@@ -3,18 +3,15 @@ package gui;
 import javax.swing.*;
 
 public class CustomDialog {
-
-    // Static method for warning messages
+    
     public static void showWarning(String message) {
         showCustomDialog(message, "Warning");
     }
-
-    // Static method for error messages
+   
     public static void showError(String message) {
         showCustomDialog(message, "Error");
     }
-
-    // Static method for informational messages
+   
     public static void showInfo(String message) {
         showCustomDialog(message, "Information");
     }
@@ -23,10 +20,9 @@ public class CustomDialog {
         int option = JOptionPane.showConfirmDialog(null, message, title, JOptionPane.YES_NO_OPTION);
         return option;
     }
-
-    // Method to create a custom dialog
+   
     private static void showCustomDialog(String message, String title) {
         CustomPopupWindow popup = new CustomPopupWindow(title, message);
-        popup.show(); // Display the custom popup window
+        popup.show();
     }
 }
